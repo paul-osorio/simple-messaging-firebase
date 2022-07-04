1357b55bf33380449064cb1a42504d9a5b270760
Paul Osorio <johnpaul.cajes.osorio@gmail.com>
2022-07-04 18:29:40 +0800

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { onAuthStateChanged, signOut } from "firebase/auth";
-import { useEffect, useState } from "react";
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./assets/css/App.css";
 import PublicRoute from "./components/PublicRoute";
 import PrivateRoute from "./components/PrivateRoute";
-import { useAuth } from "./context/AuthProvider";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Message from "./pages/message";
